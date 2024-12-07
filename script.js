d1cef30ddbbd97fc8c9638d418935574473060ee
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2024-12-07 08:23:46 -0300

    feat: camera config

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ AFRAME.registerComponent('boat-controls', {
         });
     }
 });
+
+AFRAME.registerComponent('follow', {
+    schema: {
+      target: { type: 'selector' },
+      offset: { type: 'vec3', default: { x: 0, y: 0, z: 3 } },
+    },
+    tick: function () {
+      const target = this.data.target;
+
+      // Posição do objeto alvo
+      const targetPosition = target.object3D.position;
+
+      // Atualizar posição da câmera com o offset
+      this.el.object3D.position.set(
+        targetPosition.x + this.data.offset.x,
+        targetPosition.y + this.data.offset.y,
+        targetPosition.z + this.data.offset.z
+      );
+    },
+  });
