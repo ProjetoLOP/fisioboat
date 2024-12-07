788a2e8bae19982130b7d54abe8f4069a09056e0
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2024-12-07 09:32:42 -0300

    feat: skybox and water texture

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 AFRAME.registerComponent('boat-controls', {
     init: function () {
+        // Verifica input do teclado
         window.addEventListener('keydown', (event) => {
             const position = this.el.getAttribute('position');
             switch (event.key) {
+                // Movimento para frente
                 case ' ':
                     position.z -= 0.1;
                     break;
@@ -18,16 +20,16 @@ AFRAME.registerComponent('follow', {
       offset: { type: 'vec3', default: { x: 0, y: 0, z: 3 } },
     },
     tick: function () {
-      const target = this.data.target;
+        const target = this.data.target;
 
-      // Posição do objeto alvo
-      const targetPosition = target.object3D.position;
+        // Posição do objeto alvo
+        const targetPosition = target.object3D.position;
 
-      // Atualizar posição da câmera com o offset
-      this.el.object3D.position.set(
-        targetPosition.x + this.data.offset.x,
-        targetPosition.y + this.data.offset.y,
-        targetPosition.z + this.data.offset.z
-      );
+        // Atualizar posição da câmera com o offset
+        this.el.object3D.position.set(
+            targetPosition.x + this.data.offset.x,
+            targetPosition.y + this.data.offset.y,
+            targetPosition.z + this.data.offset.z
+        );
     },
-  });
+});
